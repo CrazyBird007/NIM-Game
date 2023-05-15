e50b32ed0cfa7c015505c8c58659d66259a22b87
Albert <wissigkeit@outlook.de>
2023-05-15 13:56:49 +0200

    build whole basic style css

---
+++ script-arrays.js
@@ -0,0 +1,66 @@
+let matches = [   // die 10 streichhölzer von denen immer welche weggenommen werden
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png",
+    "./img/match.png"
+  ];
+  
+
+let kiMoves = [ //array über die 10 möglichen anzahlen der streichhölzer für die KI
+    {
+        "number": 1, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [1], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 2, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [2], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 3, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 4, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 5, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 6, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 7, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 8, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 9, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+    {
+        "number": 10, // wie viele streichhölzer liegen noch
+        "img": "./img/match.png", // streichholzbild
+        "moves": [3], // array für dei spielzüge die möglich sind für die KI
+    },
+]
